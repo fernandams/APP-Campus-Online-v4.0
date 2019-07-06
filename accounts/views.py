72e0c47ed3c7016220275dfbe0d3b49cfc78d566
fernandams <fernandamacedo.etb@gmail.com>
2019-07-06 18:46:42 -0300

    Cria usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
-from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
@@ -17,3 +19,14 @@ class _PasswordChangeView(PasswordChangeView):
 
 class _PasswordChangeDoneView(PasswordChangeDoneView):
     template_name='accounts/password_change_done.html'
+
+class CreateUser(FormView):
+    template_name = 'accounts/user_form.html'
+    form_class = UserCreationForm
+    success_url = 'usuario-inserido-com-sucesso'
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+class UserCreated(TemplateView):
+    template_name = 'accounts/user_create_done.html'

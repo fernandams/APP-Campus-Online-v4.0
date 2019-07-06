84a2a3a72209ffcae3bf17ada6b7604182068a08
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2019-07-05 22:44:59 -0300

    Inicio da modificação da página home.

--- campus_app/views.py
+++ campus_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Noticia
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-def home (request):
-    data = {}
-    data['noticias'] = Noticia.objects.all()
-    return render(request, 'campus_app/home.html', data)
+class HomeView(TemplateView):
+    template_name="campus_app/home.html"
 
-def contato(request):
-    return render(request, 'campus_app/contato.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['noticias'] = Noticia.objects.all
+        return context
+
+class ContatoView(TemplateView):
+    template_name="campus_app/contato.html"

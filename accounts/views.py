0235fba369f8f136b394cf0abf49c73bd1079bed
fernandams <fernandamacedo.etb@gmail.com>
2019-07-06 14:56:08 -0300

    Código da alteração de senha mais organizado

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
@@ -8,3 +10,10 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     template_name="accounts/dashboard.html"
     login_url = 'login'
 
+class _PasswordChangeView(PasswordChangeView):
+    template_name='accounts/edit_password.html'
+    form_class = PasswordChangeForm
+    success_url = 'senha-alterada-com-sucesso' 
+
+class _PasswordChangeDoneView(PasswordChangeDoneView):
+    template_name='accounts/password_change_done.html'

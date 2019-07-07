76a47541e2576aa2e2d989dcd530be7b8aeeff3f
fernandams <fernandamacedo.etb@gmail.com>
2019-07-07 08:09:14 -0300

    Redefir senha funcionando, agora só configurar email quando colocar em produção

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
-from .views import (DashboardView, _PasswordChangeView, _PasswordChangeDoneView, 
+from .views import (DashboardView, _PasswordChangeView, _PasswordChangeDoneView, _PasswordResetView,
+    _PasswordResetDoneView, _PasswordResetConfirmView, _PasswordResetCompleteView,
     CreateUser, UserCreated, UsersView, UpdateUser, UsersView)
 
 
@@ -11,8 +12,12 @@ urlpatterns = [
     path('painel/', DashboardView.as_view(), name='dashboard'),
     path('alterar-senha/', _PasswordChangeView.as_view(), name='edit_password'),
     path('alterar-senha/senha-alterada-com-sucesso', _PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('nova-senha/', _PasswordResetView.as_view(), name='password_reset'),
+    path('nova-senha/nova-senha-solicitada', _PasswordResetDoneView.as_view()),
+    path('confirmar-nova-senha/<uidb64>/<token>', _PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('confirmar-nova-senha/<uidb64>/senha-redefinida-com-sucesso', _PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('novo-usuario/', CreateUser.as_view(), name='new_user'),
     path('novo-usuario/usuario-inserido-com-sucesso', UserCreated.as_view()),
     path('atualizar-usuario/<int:pk>/', UpdateUser, name='update_user'),
-    path('usuarios/', UsersView.as_view(), name='users')
-]
+    path('usuarios/', UsersView.as_view(), name='users'),
+ ]

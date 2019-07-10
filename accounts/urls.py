5fd405406bd60dfa0957e2ea3fd8cb853178ec4a
fernandams <fernandamacedo.etb@gmail.com>
2019-07-10 10:04:16 -0300

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ from .views import (DashboardView, _PasswordChangeView, _PasswordChangeDoneView,
 
 urlpatterns = [
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
-    path('', LogoutView.as_view(template_name='campus_app/home.html'), name="logout"),
+    path('sair', LogoutView.as_view(template_name='campus_app/home.html'), name="logout"),
     path('painel/', DashboardView.as_view(), name='dashboard'),
     path('alterar-senha/', _PasswordChangeView.as_view(), name='edit_password'),
     path('alterar-senha/senha-alterada-com-sucesso', _PasswordChangeDoneView.as_view(), name='password_change_done'),

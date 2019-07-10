4eae275f82d1c9d5ad044697006068db727330f0
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2019-07-10 09:04:36 -0300

    RETIRADA DO GLOBAL PERMISSIONS.

--- campus_online/settings.py
+++ campus_online/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'global_permissions',
     'campus_app',
     'accounts',
     'pwa',

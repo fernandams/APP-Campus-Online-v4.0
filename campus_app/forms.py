fd8d20d4320c344238b23261a06c7e38f1d06bee
fernandams <fernandamacedo.etb@gmail.com>
2019-12-08 17:26:01 -0300

    Múltiplos autores por notícias

--- campus_app/forms.py
+++ campus_app/forms.py
@@ -1,10 +1,15 @@
 from django import forms
+from django.forms import ModelForm
+from django.forms.widgets import SelectMultiple
 from .models import Noticia
-
+from django.contrib.auth.models import User
 
 class NoticiaForm(forms.ModelForm):
-
+    usuarios = forms.ModelMultipleChoiceField(
+            widget=forms.SelectMultiple,
+            queryset=User.objects.all().order_by('first_name'),
+            required=True)
     class Meta:
         model = Noticia
-        fields = ['cod_usuario', 'titulo', 'texto', 'prioridade',
+        fields = ['usuarios', 'titulo', 'texto', 'prioridade',
                   'link_externo', 'link_video', 'link_foto']
